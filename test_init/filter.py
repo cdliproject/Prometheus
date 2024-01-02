d9b900b6a5c790fb082d3cb7b8cb3b8b5b7c5533
rekurrenzk <Fatihburak@protonmail.com>
2024-01-02 16:24:25 +0300

    Refactor XML parsing code and add Disease class

--- test_init/filter.py
+++ test_init/filter.py
@@ -1,5 +1,3 @@
-
-
 """
 url = 'https://stackoverflow.com/questions/2869564/xml-filtering-with-python'
 
@@ -23,58 +21,82 @@ We are going to use the ElementTree lib to parse the XML file.
 The data 'en_product3_181.xml' is a 4.1MB for rare neurological diseases.
 
 """
-
-import xml.etree.ElementTree as ET
 import pandas as pd
+import xml.etree.ElementTree as ET
 
-class TestParser:
+class Disease:
     
-    def __init__(self, file):
-        self.file = file
-        self.tree = ET.parse(self.file)
+    def __init__(self,
+                 classification_id, disorder_type, disorder_id,
+                 orpha_code, name, expert_link, meta_data):
+        
+        self.classification_id = classification_id
+        self.disorder_id = disorder_id
+        self.disorder_type = disorder_type
+        self.orpha_code = orpha_code
+        self.name = name
+        self.expert_link = expert_link
+        self.meta_data = meta_data
+        
+    def to_dict(self):
+        return {
+            'ClassificationID': self.classification_id,
+            'DisorderID': self.disorder_id,
+            'DisorderType' : self.disorder_type,
+            'OrphaCode': self.orpha_code,
+            'Name': self.name,
+            'ExpertLink': self.expert_link,
+            'MetaData': self.meta_data
+        }
+        
+class OrphanetXMLParser:
+    def __init__(self,xml_file): # tree structure of the xml file
+        self.tree = ET.parse(xml_file)
         self.root = self.tree.getroot()
+        self.meta_data = self.extract_meta_data()
         
-    def parse_element(self, element):
-        element_data = {"tag": element.tag, "text": element.text.strip() if element.text else None, "attributes": element.attrib}
-        children_data = []
+    def extract_meta_data(self):
+        """
+        will be attributing meta_data from XML file to further
+        analyse the data and find the most relevant interlinks
+        for our project.
+        These meta_datas can be Licence information, date of JDBOR,
+        or Orphanumber (dont confuse it with orpha code).
+        """
         
-        for child in element:
-            child_data = self.parse_element(child)
-            children_data.append(child_data)
-            
-        if children_data:
-            element_data["children"] = children_data
-            
-        return element_data
-    
-    def parse_root(self):
-        return self.parse_element(self.root)
-    
-    def flatten_data(self, element, path=''):
-        flat_data = []
-        current_path = f"{path}/{element['tag']}"
-        
-        if 'children' not in element:
-            return [{**element['attributes'], 'path': current_path, 'text': element['text']}]
-        
-        for child in element['children']:
-            child_data = self.flatten_data(child, path=current_path)
-            flat_data.extend(child_data)
-            
-        return flat_data
-    
-        
-parser = TestParser('en_product3_181.xml')
-parsed_data = parser.parse_root()
-flat_data = parser.flatten_data(parsed_data)
+        return {
 
-df = pd.DataFrame(flat_data)
-df.to_excel('output.xlsx', index=False)
-df.to_csv('output.csv', index=False)
-#here I refined both csv and xlsx formats to compare the differences, if any.
+        }
+        
+    def parse_diseases(self):
+        diseases = [] # list of diseases
+        
+        for disorder in self.root.findall('.//Disorder'): # find all the disorders, nested in the root 
 
+            classification_id = disorder.get('id', 'Unknown')
+            disorder_id = disorder.get('id', 'Unknown')
+            disorder_type = disorder.get('id', 'Unknown')
+            
+            orpha_code = disorder.find('OrphaCode').text\
+                if disorder.find('OrphaCode') is not None else 'Unknown' # find the orpha code, nested in the disorder
+            
+            name = disorder.find('Name').text\
+                if disorder.find('Name') is not None else 'Unknown' # find the name, nested in the disorder
+                
+            expert_link = disorder.find('ExpertLink').text\
+                if disorder.find('ExpertLink') is not None else 'Unknown' # find the expert link, nested in the disorder
+            
+            disease =\
+                Disease(self.meta_data,classification_id, disorder_id,\
+                    disorder_type, orpha_code, name, expert_link)    # create a disease object
+            
+            diseases.append(disease)
+            
+        return diseases
+    
+parser = OrphanetXMLParser('en_product3_181.xml')
 
-"""
-This method is very unideal because it is not scalable. 
-Will try to find a better way to parse the XML file.
-"""
+diseases = parser.parse_diseases()
+
+diseases_df = pd.DataFrame([disease.to_dict() for disease in diseases])
+print(diseases_df)
